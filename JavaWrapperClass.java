223ac4dc7929199a21d427690130660fe467dfba
Dineshkumar Anbalagan <dinesh2172@outlook.com>
2021-11-22 22:26:49 +0530

    wrapper and primitive

---
+++ JavaWrapperClass.java
@@ -0,0 +1,30 @@
+public class JavaWrapperClass{
+    public static void main(String[] args){
+
+        // WRAPPER CLASSES  = Provides a way to use primitive datatypes as reference datatypes 
+        //                    reference datatypes contains useful methods which primitive datatypes don't
+        //                    can be used with collections (ex. ArrayList)
+
+        //primitive         //wrapper
+        //------------      //------------
+        // boolean          // Boolean
+        // char             // Character
+        // int              // Integer
+        // double           // Double
+
+        // autoboxing = the automatic conversion that the java compiler makes between the primitive types and their corresponding object wrapper class.
+        // unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive.
+
+        // using wrappers ==> we can use wrapper class in the same way as primitive.
+        
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 12;
+        Double d = 3.14;
+        String e = "Dinesh";
+
+
+
+    }
+    
+}

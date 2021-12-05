3b22ce0cbc47f9d1be7a45e5a9c7799d5e02fb5a
Dineshkumar Anbalagan <dinesh2172@outlook.com>
2021-12-05 22:04:52 +0530

    commit on 'git'

---
+++ V3 - Java OOPS Variable Scope/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //local variables and objects   = declared inside a method
+        //                                visible only to that method
+
+        //global variables and objects  = declared outside a method, but within a class
+        //                                visible to all parts of a class.
+
+        DiceRoller diceroller = new DiceRoller();
+
+    }
+
+
+}
